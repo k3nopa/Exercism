e37ea7670e034a723e5a070fc66c66301e87872d
k3nopa <afiqnaufalpro@gmail.com>
2021-10-02 00:44:25 +0900

    remove magazine-cutout exercise

--- Rust/magazine-cutout/target/debug/libmagazine_cutout.d
+++
@@ -1,1 +0,0 @@
-/Users/afiqnaufal/Documents/programming/Exercism/Rust/magazine-cutout/target/debug/libmagazine_cutout.rlib: /Users/afiqnaufal/Documents/programming/Exercism/Rust/magazine-cutout/src/lib.rs
